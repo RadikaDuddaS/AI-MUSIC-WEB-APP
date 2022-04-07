56e09688e651853e3ecf91e46401fc4d5e860c1d
RadikaDuddaS <102700155+RadikaDuddaS@users.noreply.github.com>
2022-04-07 19:03:16 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,41 @@
+song = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
-song1 = "";
-song2 = "";
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function preload()
 {
-    song1 = loadSound("music.mp3");
-    song2 = loadSound("music2.mp3");
+    song = loadSound("music.mp3")
 }
 
-function setup()
+function setup () 
 {
     canvas = createCanvas(600,500);
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide()
+    video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on('pose', gotPoses);
 }
 
-function gotPoses(results) 
+function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+
+        console.log("scoreLeftWrist = " + scoreLeftWrist);
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
-        console.log("leftWristX = " + leftWristX + "leftWristY = " +  leftWristY);  
+        console.log("leftWristX = " + leftWristX + "leftWristY = " +  leftWristY);
     
         rightWristX = results[0].pose.rightWrist.x;
         rightWristY = results[0].pose.rightWrist.y;
@@ -46,9 +51,57 @@ function modelLoaded()
 function draw()
 {
     image(video,0,0,600,500);
+
+    fill("pink");
+    stroke("black");
+
+    circle(rightWristX, rightWristY, 20);
+
+    if(scoreRightWrist > 0.2)
+    {
+        if(rightWristY > 0 && rightWristY <= 100)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 0.5x";
+            song.rate(0.5);
+        }
+    
+        else if(rightWristY >100 && rightWrist <=200)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1x";
+            song.rate(1);
+        }
+     
+        else if(rightWristY > 200 && rightWrist <=300)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 1.5x";
+            song.rate(1.5);
+        }
+    
+        else if(rightWristY >300 && rightWrist <=400)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2x";
+            song.rate(2);
+        }
+    
+        else if(rightWristY >400 && rightWrist <=500)
+        {
+            document.getElementById("speed").innerHTML = "Speed = 2.5x";
+            song.rate(2.5)
+        }     
+    }
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimals/500;
+        document.getElementById("volume").innerHTML = "Volume = " + volume;
+        song.setVolume(volume);
+    }
 }
 
-function play()
+function play() 
 {
     song.play();
     song.setVolume(1);
